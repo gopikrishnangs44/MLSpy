515a7f234034504db33fab0397650265ed7160db
Gopikrishnan G S <59282012+gopikrishnangs44@users.noreply.github.com>
2024-01-31 17:52:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='MLSpy',
-    version='0.0.2',
+    version='1.0.0',
     author='Gopikrishnan G S',
     distribution='Python toolkit for MLS Ozone',
     packages=['MLSpy'],
